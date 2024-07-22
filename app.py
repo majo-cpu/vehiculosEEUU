f8bce64c572226fe4dfdb7e7a30145d2b4dd2209
majo-cpu <m.joseathuan@gmail.com>
2024-07-21 20:48:22 -0400

    Update app.py
    2048

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import win32api
-car_data = pd.read_csv(r'C:\Users\nicol\OneDrive\Escritorio\vehicles_env\vehicles_us.csv', sep=',')
-print(car_data)
+import plotly_express as px
 
-# Display the DataFrame (optional)
-st.write(car_data)
+st.header('Histograma y Dispersion en funcion de el kilometraje de un vehiculo')
 
-# Create a button for building the histogram
-hist_button = st.button('Construir histograma')
+# 
 
-# Check if the button is clicked
-if hist_button:
-    # Display a message indicating the histogram creation
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    
-    # Create a histogram using Plotly Express
-    fig = px.histogram(car_data, x="odometer")
-    
-    # Display the Plotly chart
+car_data = pd.read_csv("./data/vehicles_us.csv")  # leer los datos
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir dispersión')
+
+
+## Plots
+if build_histogram:
+    st.write('Histograma de anuncios de venta de coches')
+
+    # Crear histograma
+
+    fig = px.histogram(car_data, x='odometer')
+
+    # Mostrar grafico plotly interactivo
+
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Construir gráfico de dispersión')
+if build_scatter:
+    st.write( 'Dispersión para los datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x='odometer', y="price")
 
-# Verificar si se hizo clic en el botón del gráfico de dispersión
-if scatter_button:
-    # Mostrar un mensaje indicando la creación del gráfico de dispersión
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
-    
-    # Crear un gráfico de dispersión utilizando Plotly Express
-    fig = px.scatter(car_data,x="model_year", y="price", color="fuel")
-    
-    # Mostrar el gráfico de Plotly
     st.plotly_chart(fig, use_container_width=True)
